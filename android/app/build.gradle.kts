f8b774f76b7863d9dbccb81d488c7ef17b19256f
Safal Shrestha <safalshrestha721156@gmail.com>
2025-06-10 11:31:03 +0545

    deleted unncessary files

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.verygoodcore.my_game"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.verygoodcore.my_game"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    flavorDimensions += "default"
+
+productFlavors {
+    create("production") {
+        dimension = "default"
+        applicationIdSuffix = ""
+        manifestPlaceholders["appName"] = "My Game"
+    }
+    create("staging") {
+        dimension = "default"
+        applicationIdSuffix = ".stg"
+        manifestPlaceholders["appName"] = "[STG] My Game"
+    }
+    create("development") {
+        dimension = "default"
+        applicationIdSuffix = ".dev"
+        manifestPlaceholders["appName"] = "[DEV] My Game"
+    }
+}
+
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
